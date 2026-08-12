375e3d790dd502eec84c80d582b5498578aa8f90
Chaitanya <160163592+chaitanya-0801@users.noreply.github.com>
2026-08-12 22:25:58 +0530

    Update logo text from 'AttendEase' to 'Attendo'

--- frontend/src/pages/Nav.js
+++ frontend/src/pages/Nav.js
@@ -44,7 +44,7 @@ const Nav = () => {
               className="logo-img"
             />
             <span className="logo-text">
-              AttendEase
+              Attendo
             </span>
           </a>
         </div>
